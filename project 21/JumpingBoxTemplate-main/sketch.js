2b532c4b95fac7c1cdab2b613017378c5543de55
RGouthamKrishna <76640070+RGouthamKrishna@users.noreply.github.com>
2021-04-24 20:02:14 +0530

    Add files via upload

---
+++ project 21/JumpingBoxTemplate-main/sketch.js
@@ -0,0 +1,64 @@
+var canvas;
+var music;
+var ball;
+var ground1;
+var ground2;
+var ground3;
+var ground4;
+
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+ball = createSprite(10,50,20,20);
+    //create 4 different surfaces
+    ground1 = createSprite(90,580,180,20);
+    ground2 = createSprite(300,580,180,20);
+    ground3 = createSprite(510,580,180,20);
+    ground4 = createSprite(720,580,180,20);
+
+
+
+    //create box sprite and give velocity
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    createEdgeSprites();
+
+    ground1.shapeColor = "green";
+    ground2.shapeColor = "yellow";
+    ground3.shapeColor = "red";
+    ground4.shapeColor = "blue";
+    ball.shapeColor = "white";
+
+    ball.bounceOff(ground1);
+    ball.bounceOff(ground2);
+    ball.bounceOff(ground3);
+    ball.bounceOff(ground4);
+    ball.bounceOff(edges);
+
+    
+    
+
+
+    
+        
+
+
+ 
+    ball.velocityX = 4;
+    ball.velocityY = 4;
+    drawSprites();
+
+    //create edgeSprite
+
+
+
+    //add condition to check if box touching surface and make it box
+}
